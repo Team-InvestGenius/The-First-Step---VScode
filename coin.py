0af915955c28ff61207fd959db074e83faff7bd3
이성도 <dltjdeh7745@naver.com>
2024-07-05 09:18:30 +0900

    coin.py 업데이트

--- coin.py
+++ coin.py
@@ -1,17 +1,43 @@
+import os
 import requests
+import csv
+import time
+from datetime import datetime
 
-# 코인개코에서 API 로 비트코인,이더리움,리플 가격을 받아옴
-def get_crypto_prices_in_krw(crypto_ids):
+def get_crypto_prices_in_krw(crypto_ids, api_key):
     url = 'https://api.coingecko.com/api/v3/simple/price'
+    headers = {
+        "accept": "application/json",
+        "x-cg-demo-api-key": api_key
+    }
     params = {
         'ids': ','.join(crypto_ids),
         'vs_currencies': 'krw'
     }
-    response = requests.get(url, params=params)
+    response = requests.get(url, headers=headers, params=params)
     return response.json()
 
-crypto_ids = ['bitcoin', 'ethereum', 'ripple']
-prices = get_crypto_prices_in_krw(crypto_ids)
+def write_to_csv(data, filename='./data/crypto_data.csv'):
+    fieldnames = ['timestamp', 'crypto', 'price_krw']
+    file_exists = os.path.isfile(filename)
 
-for crypto, price_info in prices.items():
-    print(f"{crypto.capitalize()} price in KRW: {price_info['krw']}")
+    with open(filename, mode='a', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        
+        if not file_exists:
+            writer.writeheader()
+        
+        for crypto, price_info in data.items():
+            writer.writerow({
+                'timestamp': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                'crypto': crypto,
+                'price_krw': price_info['krw']
+            })
+
+api_key = 'CG-xc9KizrBdzQEjwCE776DeTC9'
+crypto_ids = ['bitcoin', 'ethereum', 'ripple']
+
+while True:
+    prices = get_crypto_prices_in_krw(crypto_ids, api_key)
+    write_to_csv(prices)
+    time.sleep(30)
