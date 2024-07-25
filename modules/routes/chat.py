ab8e5b27ac87215c37475c9e26132e233ea7a463
rudtkd1121 <qwo5qwo5@gmail.com>
2024-07-25 07:20:12 +0000

    chat.py change

--- modules/routes/chat.py
+++ modules/routes/chat.py
@@ -17,7 +17,7 @@ if not openai_api_key:
 gpt_model = GPTModel(api_key=openai_api_key, model_id="gpt-3.5-turbo")
 
 # 키워드를 변수로 저장
-KEYWORDS = ["죄송합니다", "모르겠습니다", "잘 모르겠","gpt help"]
+KEYWORDS = ["죄송합니다", "모르겠습니다", "잘 모르겠", "gpt help"]
 
 # 서버 시작 시 LlamaModel 초기화
 llama_model = LlamaModel()
@@ -31,30 +31,36 @@ def ask_question():
     try:
         data = request.get_json()
         question = data.get('question')
-        chatroom_id = data.get('chatroom_id')
         user_id = data.get('user_id')
 
-        if not question or not chatroom_id or not user_id:
-            return jsonify({'error': 'No question, chatroom_id or user_id provided'}), 400
+        if not question or not user_id:
+            return jsonify({'error': 'question, or user_id 제공되지 않음'}), 400
 
         db_connector = ChatDBConnector()
-        try:
-            chat_history = db_connector.get_chat_history(chatroom_id)
-            formatted_chat_history = "\n".join([f"{entry['speaker']}: {entry['message']}" for entry in chat_history])
-            
-            llama_response = llama_model.generate_response(f"{formatted_chat_history}\nUser: {question}")
+        # 유저의 채팅룸 개수 확인 및 채팅방 생성
+        chatroom_count = db_connector.get_chatroom_count_by_userid(user_id)
+        if chatroom_count >= 3:  # 채팅방 3개 이상이면
+            chatroom_id = db_connector.get_last_active_chatroom_by_userid(user_id)
+        else:
+            chatroom_id = db_connector.create_chatroom(user_id)
 
-            if evaluate_response(llama_response, KEYWORDS):
-                gpt_response = gpt_model.generate(f"{formatted_chat_history}\nUser: {question}")
-                db_connector.save_chat_history(chatroom_id, 'user', question)
-                db_connector.save_chat_history(chatroom_id, 'gpt', gpt_response)
-                return jsonify({'response': gpt_response})
-            else:
-                db_connector.save_chat_history(chatroom_id, 'user', question)
-                db_connector.save_chat_history(chatroom_id, 'llama', llama_response)
-                return jsonify({'response': llama_response})
-        finally:
-            db_connector.close()
+        chat_history = db_connector.get_chat_history(chatroom_id)
+        formatted_chat_history = "\n".join([f"{entry['speaker']}: {entry['message']}" for entry in chat_history])
+
+        llama_response = llama_model.generate_response(f"{formatted_chat_history}\nUser: {question}")
+
+        if evaluate_response(llama_response, KEYWORDS):
+            gpt_response = gpt_model.generate(f"{formatted_chat_history}\nUser: {question}")
+            db_connector.save_chat_history(chatroom_id, 'user', question)
+            db_connector.save_chat_history(chatroom_id, 'gpt', gpt_response)
+            response = {'response': gpt_response, 'chatroom_id': chatroom_id}
+        else:
+            db_connector.save_chat_history(chatroom_id, 'user', question)
+            db_connector.save_chat_history(chatroom_id, 'llama', llama_response)
+            response = {'response': llama_response, 'chatroom_id': chatroom_id}
+
+        db_connector.close()  
+        return jsonify(response)
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
@@ -69,10 +75,8 @@ def delete_chatroom():
             return jsonify({'error': '채팅방 ID와 유저 ID가 제공되지 않았습니다.'}), 400
 
         db_connector = ChatDBConnector()
-        try:
-            db_connector.delete_chatroom(chatroom_id)
-        finally:
-            db_connector.close()
+        db_connector.delete_chatroom(chatroom_id)
+        db_connector.close()  
 
         return jsonify({'message': '채팅방 삭제 완료', 'chatroom_id': chatroom_id})
     except Exception as e:
@@ -85,10 +89,8 @@ def get_history():
         return jsonify({'error': '채팅방 ID가 제공되지 않았습니다.'}), 400
 
     db_connector = ChatDBConnector()
-    try:
-        chat_history = db_connector.get_chat_history(chatroom_id)
-    finally:
-        db_connector.close()
+    chat_history = db_connector.get_chat_history(chatroom_id)
+    db_connector.close()  
 
     return jsonify({'chat_history': chat_history})
 
