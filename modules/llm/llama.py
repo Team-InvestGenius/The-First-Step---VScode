c9ab73c4b53c9eb7b0799f20e6c9a11728f1839d
Joohyoung Jeon <jeonjoohyoung@gmail.com>
2024-07-26 14:40:18 +0900

    debug: remove minor

--- modules/llm/llama.py
+++ modules/llm/llama.py
@@ -7,7 +7,9 @@ from flask import current_app
 from modules.llm.utils import PROMPT
 
 
-quantization_config = BitsAndBytesConfig(load_in_4bit=True)
+def clear_cuda_cache():
+    if torch.cuda.is_available():
+        torch.cuda.empty_cache()
 
 
 class LlamaModel:
@@ -26,16 +28,17 @@ class LlamaModel:
         self.tokenizer = None
         self.model = None
 
-    @lru_cache(maxsize=3)
+    @lru_cache(maxsize=1)
     def load_model(self):
         if self.tokenizer is None or self.model is None:
             model_id = current_app.config['MODEL_ID']
             model_path = current_app.config["MODEL_PATH"]
             self.tokenizer = AutoTokenizer.from_pretrained(model_id)
             self.model = AutoModelForCausalLM.from_pretrained(
-                model_path,
-                quantization_config=quantization_config,
-                torch_dtype=torch.bfloat16).to(self.device)
+                model_id,
+                torch_dtype=torch.bfloat16,
+                device_map="auto"
+            )
         return self.model, self.tokenizer
 
     def generate_response(self, instruction):
@@ -47,4 +50,9 @@ class LlamaModel:
         input_ids = tokenizer.apply_chat_template(messages, add_generation_prompt=True, return_tensors="pt").to(self.device)
         outputs = model.generate(input_ids, max_new_tokens=2048, eos_token_id=tokenizer.eos_token_id, do_sample=True, temperature=0.6, top_p=0.9)
         response = tokenizer.decode(outputs[0][input_ids.shape[-1]:], skip_special_tokens=True)
+
+        # CUDA 캐시 정리
+        if torch.cuda.is_available():
+            torch.cuda.empty_cache()
+
         return response
